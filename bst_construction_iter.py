d1727bb38a99d83288e97b9b120e255294c1aac2
Jon Wehner <jonjwehner@gmail.com>
2021-09-20 18:24:39 -0700

    bst iterative

--- bst_construction_iter.py
+++ bst_construction_iter.py
@@ -9,61 +9,70 @@ class BST:
         self.right = None
 
     def insert(self, value):
-		while True:
-			if value < self.value:
-				if self.left is None:
-					self.left = BST(value)
-					break
-				else:
-					self = self.left
-			else:
-				if self.right is None:
-					self.right = BST(value)
-					break
-				else:
-					self = self.right
+        currentNode = self
+        while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
         return self
-	
-    def contains(self, value):        
-		while self is not None:
-			if value < self.value:
-				self = self.left
-			elif value > self.value:
-				self = self.right
-			else:
-				return True
-		return False
 
-    def remove(self, value, parent=None):
-        if value < self.value:
-			if self.left is not None:
-				self.left.remove(value, self)
-		elif value > self.value:
-			if self.right is not None:
-				self.right.remove(value, self)
-		else:
-			if self.left is not None and self.right is not None:
-				self.value = self.right.getMinValue()
-				self.right.remove(self.value, self)
-			elif parent is None:
-				if self.left is not None: 
-					self.value = self.left.value
-					self.right = self.left.right
-					self.left = self.left.left
-				elif self.right is not None:
-					self.value = self.right.value
-					self.left = self.right.left
-					self.right = self.right.right
-				else:
-					pass
-			elif parent.left == self:
-					parent.left = self.left if self.left is not None else self.right
-			elif parent.right == self:
-					parent.right = self.left if self.left is not None else self.right 
+    def contains(self, value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+        return False
+
+    def remove(self, value, parentNode=None):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.right
+            else:
+                if (currentNode.left
+                        is not None and currentNode.right is not None):
+                    currentNode.value = currentNode.right.getMinValue()
+                    currentNode.right.remove(
+                        currentNode.value, currentNode)
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left = currentNode.left.left
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.left
+                        currentNode.right = currentNode.right.right
+                    else:
+                        pass
+                elif parentNode.left == currentNode:
+                    parentNode.left = (currentNode.left if currentNode.left
+                                       is not None else currentNode.right)
+                elif parentNode.right == currentNode:
+                    parentNode.right = (currentNode.left if currentNode.left
+                                        is not None else currentNode.right)
+                break
         return self
-	
-	def getMinValue(self):
-		if self.left is None:
-			return self.value
-		else:
-			return self.left.getMinValue()
+
+        def getMinValue(self):
+            currentNode = self
+            while currentNode.left is not None:
+                currentNode = currentNode.left
+            return currentNode.value
