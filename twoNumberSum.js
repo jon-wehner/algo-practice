e65a81a7dedc3ac573972bc07fb6be90620be1b2
Jon Wehner <jonjwehner@gmail.com>
2021-11-30 13:31:54 -0800

    two number sum js

---
+++ twoNumberSum.js
@@ -0,0 +1,29 @@
+function twoNumberSumHash (array, targetSum) {
+  const nums = {};
+  for (let i = 0; i < array.length; i++) {
+    const potentialMatch = targetSum - array[i];
+    if (nums[potentialMatch]) {
+      return [potentialMatch, array[i]]
+    } else {
+      nums[array[i]] = true;
+    }
+  };
+  return []
+};
+
+function twoNumberSumPointers (array, targetSum) {
+  array.sort();
+  const leftPointer = 0;
+  const rightPointer = array.length - 1;
+  while (leftPointer < rightPointer) {
+    const currentSum = array[leftPointer] + array[rightPointer];
+    if (currentSum == targetSum) {
+      return [array[leftPointer], array[rightPointer]]
+    } else if (currentSum < targetSum) {
+      leftPointer += 1
+    } else if (currentSum > targetSum) {
+      rightPointer += 1
+    }
+  }
+  return []
+}
