3826b87e333b565a02f6879d53bc817a3db09908
Jon Wehner <jonjwehner@gmail.com>
2021-12-01 16:21:40 -0800

    tournament winner

---
+++ tournamentWinner.js
@@ -0,0 +1,27 @@
+const homeTeamWon = 1;
+
+function tournamentWinner (competitions, results) {
+  let currentBestTeam = "";
+  const scores = {currentBestTeam: 0};
+
+  for (let i = 0; i < competitions.length; i++) {
+    const result = results[i];
+    [homeTeam, awayTeam] = competitions[i];
+    const winningTeam = result === homeTeamWon ? homeTeam : awayTeam;
+
+    updateScores(winningTeam, 3, scores);
+
+    if (scores[winningTeam] > scores[currentBestTeam]) {
+      currentBestTeam = winningTeam;
+    };
+
+  };
+};
+
+function updateScores(team, score, scores) {
+  if (scores[team]) {
+    scores[team] += score;
+  } else {
+    scores[team] = score
+  };
+};
