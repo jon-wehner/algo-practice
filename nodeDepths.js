09e923f498f1eee56e74ec4ba9b9c4245129854a
Jon Wehner <jonjwehner@gmail.com>
2022-01-04 19:41:24 -0800

    node depths

--- nodeDepths.js
+++ nodeDepths.js
@@ -13,3 +13,9 @@ function nodeDepths(root) {
 
   };
 };
+
+function nodeDepths(root, depth=0) {
+  // base case
+  if (root === null) return 0
+  return depth + nodeDepths(root.left, depth + 1) + (nodeDepths(root.right), depth + 1)
+}
