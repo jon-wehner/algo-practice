c8e08995588371cfa26a734dc75429e3edc31369
Jon Wehner <jonjwehner@gmail.com>
2022-01-07 09:57:32 -0800

    shirt color fixesd

--- classPhotos.js
+++ classPhotos.js
@@ -2,21 +2,15 @@ function clasSPhotos(redShirts, blueShirts) {
   redShirts.sort((a,b) => b - a)
   blueShirts.sort((a,b) => b - a)
   let shirtColorInFirstRow =''
-  redShirts[0] < blueShirts[0] ? shirtColorInFirstRow = 'RED' : shirtColorInFirstRow = 'BLUE';
+  const shirtColorInFirstRow = redShirts[0] < blueShirts[0] ? 'RED' : 'BLUE';
 
   for (let i = 0; i < redShirts.length; i++) {
     const redShirtHeight = redShirts[i];
     const blueShirtHeight = blueShirts[i];
 
     if (shirtColorInFirstRow == 'RED') {
-      if (redShirtHeight >= blueShirtHeight) {
-        return false
-      } else {
-        if (blueShirtHeight >= redShirtHeight) {
-          return false
-        }
-      }
-    }
+      if (redShirtHeight >= blueShirtHeight) return false
+    } else if (blueShirtHeight >= redShirtHeight) return false
   }
   return true
 }
