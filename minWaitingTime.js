d99d41cdaa5a0b0defb265f8d44ceb1eba874815
Jon Wehner <jonjwehner@gmail.com>
2022-01-07 08:59:03 -0800

    min waiting time javascript

---
+++ minWaitingTime.js
@@ -0,0 +1,10 @@
+function minWaitingTime(array) {
+  array.sort();
+  let currWaitTime = 0
+  let totalWaitTime = 0
+  for (const time of array) {
+    totalWaitTime += currWaitTime;
+    currWaitTime += time;
+  };
+  return totalWaitTime;
+};
