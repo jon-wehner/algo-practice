b0bdc09f41f63d81ec794e804021a750cc870d34
Jon Wehner <jonjwehner@gmail.com>
2022-01-07 09:47:42 -0800

    class photos

---
+++ classPhotos.js
@@ -0,0 +1,22 @@
+function clasSPhotos(redShirts, blueShirts) {
+  redShirts.sort((a,b) => b - a)
+  blueShirts.sort((a,b) => b - a)
+  let shirtColorInFirstRow =''
+  redShirts[0] < blueShirts[0] ? shirtColorInFirstRow = 'RED' : shirtColorInFirstRow = 'BLUE';
+
+  for (let i = 0; i < redShirts.length; i++) {
+    const redShirtHeight = redShirts[i];
+    const blueShirtHeight = blueShirts[i];
+
+    if (shirtColorInFirstRow == 'RED') {
+      if (redShirtHeight >= blueShirtHeight) {
+        return false
+      } else {
+        if (blueShirtHeight >= redShirtHeight) {
+          return false
+        }
+      }
+    }
+  }
+  return true
+}
