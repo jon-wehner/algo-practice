6011d30a8281784567ffb6003dd17d075fe0e3b9
Jon Wehner <jonjwehner@gmail.com>
2022-01-11 21:52:49 -0800

    distinct nodes

---
+++ removeDuplicates.js
@@ -0,0 +1,24 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+  let currentNode = linkedList;
+	while( currentNode !== null) {
+		let nextDistinctNode = currentNode.next;
+		while (nextDistinctNode !== null && nextDistinctNode.value == currentNode.value) {
+			mextDistinctNode = nextDisctingNode.next
+		}
+		currentNode.next = nextDistinctNode;
+		currentNode = nextDistinctNode
+	}
+  return linkedList;
+}
+
+// Do not edit the lines below.
+exports.LinkedList = LinkedList;
+exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
