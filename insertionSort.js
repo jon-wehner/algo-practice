fdad58e6145f12c42b2b08afbcc74990d4312154
Jon Wehner <jonjwehner@gmail.com>
2022-01-19 22:51:48 -0800

    insertion

---
+++ insertionSort.js
@@ -0,0 +1,17 @@
+// implement insertion sort
+// time:  O(N^2) space: O(1)
+
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i
+    while ( j > 0 && array[j] < array[j - 1]) {
+      swap(j, j - 1, array);
+      j -= 1;
+    };
+  };
+  return array;
+};
+
+function swap(i, j, array) {
+  [array[i], array[j]] = [array[j], array[i]]
+};
