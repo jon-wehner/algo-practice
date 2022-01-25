6a8a401f4967d55668c8cc2a98400551c63b8de0
Jon Wehner <jonjwehner@gmail.com>
2022-01-24 23:54:32 -0800

    character counts

---
+++ generateDocument.js
@@ -0,0 +1,16 @@
+function generateDocument(characters, document) {
+  const charCounts = {};
+
+  for (const character of characters) {
+    if(!(character in charCounts)) charCounts[character] = 0;
+
+    charCounts[character]++;
+  }
+
+  for (const character of document) {
+    if(!(character in charCounts) || charCounts[character] === 0) return false;
+
+    charCounts[character]--
+  }
+  return true;
+}
