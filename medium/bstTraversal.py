bc67ffe8c4be70500a84adecb326c0ee45ba082e
Jon Wehner <jonjwehner@gmail.com>
2022-03-08 23:38:58 -0800

    forgot paramaters

--- medium/bstTraversal.py
+++ medium/bstTraversal.py
@@ -9,14 +9,14 @@ def inOrderTraverse(tree, array):
 def preOrderTraverse(tree, array):
     if tree is not None:
         array.append(tree.value)
-        preOrderTraverse(tree.left)
-        preOrderTraverse(tree.right)
+        preOrderTraverse(tree.left, array)
+        preOrderTraverse(tree.right, array)
     return array
 
 
 def postOrderTraverse(tree, array):
     if tree is not None:
-        postOrderTraverse(tree.left)
-        postOrderTraverse(tree.right)
+        postOrderTraverse(tree.left, array)
+        postOrderTraverse(tree.right, array)
         array.append(tree.value)
     return array
