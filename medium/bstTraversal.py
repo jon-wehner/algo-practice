3f48df512bee4d277c7b2c5601657e5f0d595522
Jon Wehner <jonjwehner@gmail.com>
2022-03-08 23:37:56 -0800

    bst traversal

---
+++ medium/bstTraversal.py
@@ -0,0 +1,22 @@
+def inOrderTraverse(tree, array):
+    if tree is not None:
+        inOrderTraverse(tree.left, array)
+        array.append(tree.value)
+        inOrderTraverse(tree.right, array)
+    return array
+
+
+def preOrderTraverse(tree, array):
+    if tree is not None:
+        array.append(tree.value)
+        preOrderTraverse(tree.left)
+        preOrderTraverse(tree.right)
+    return array
+
+
+def postOrderTraverse(tree, array):
+    if tree is not None:
+        postOrderTraverse(tree.left)
+        postOrderTraverse(tree.right)
+        array.append(tree.value)
+    pass
